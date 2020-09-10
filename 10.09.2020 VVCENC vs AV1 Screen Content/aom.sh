1cc8a2692359eaa0f5371ccc5c7a3b406fb6164a
Zen <46526140+master-of-zen@users.noreply.github.com>
2020-09-10 19:26:37 +0300

    update aom scripts

--- 10.09.2020 VVCENC vs AV1 Screen Content/aom.sh
+++ 10.09.2020 VVCENC vs AV1 Screen Content/aom.sh
@@ -1,20 +1,24 @@
-#!/bin/bash
-for cpu in 6
-do
-    for i in 50 40 30 20
-    do
-        runtime=`av1an.py -fmt yuv420p10le -p 1 -s 0 -i ${1} -v " -b 10 --threads=12 --end-usage=q --cq-level=$i --cpu-used=$cpu " -o aom${i}_${c} | grep Finished | cut -d' ' -f2 | tr -d '[[:alpha:]]'`
-        vmaf=`ffmpeg -r 60 -i aom${i}_${c}.mkv -r 60 -i ${1} -filter_complex libvmaf=psnr=1:ssim=1:ms_ssim=1:log_path=${i}_${c}.json:log_fmt=json -f null - `
+j=0
 
-        vmaf=`jq '.["VMAF score"]'  ${i}_${c}.json`
-        psnr=`jq '.["PSNR score"]'  ${i}_${c}.json`
-        ssim=`jq '.["SSIM score"]'  ${i}_${c}.json`
-        ms_ssim=`jq '.["MS-SSIM score"]'  ${i}_${c}.json`
-
-        bitrate=`ffprobe -i aom${i}_${c}.mkv 2>&1 | grep bitrate | rev | cut -d' ' -f2 | rev`
-        output="('aom', $runtime, $cpu, $i, $bitrate, $vmaf, $psnr, $ssim, $ms_ssim),"
-        eoutput=""
-        printf "('aom', %s, %s, %d, %s, %s, %s, %s, %s)," "$runtime" "$cpu" "$i" "$bitrate" "$vmaf" "$psnr" "$ssim" "$ms_ssim" | tee -a "aom_${1}data.txt"
+for cpu in 6 5 4 3 2 1 0 ; do
+    for i in 60 55 50 45 40 35 30 25; do
+        temp_folder=folder-$j
+cat <<EOF
+        runtime=\$(av1an -fmt yuv420p10le -p 1 -s 0 -i "${1}" -v " --threads=12 --end-usage=q --cq-level=$i --cpu-used=${cpu} " --temp aom${cpu}_${i} -o "aom${cpu}_${i}" | grep Finished | cut -d' ' -f2 | tr -d '[:alpha:]'); \
+        ffmpeg -nostdin -r 60 -i "aom${cpu}_${i}.mkv" -r 60 -i "${1}" -filter_complex "libvmaf=psnr=1:ssim=1:ms_ssim=1:log_path=${i}_${j}.json:log_fmt=json" -f null - 2> /dev/null; \
+        printf "('%s', %s, %s, %s, %s, %s, %s, %s, %s)," \
+            "aom" \
+            "\$runtime" \
+            "${cpu}" \
+            "$i" \
+            "\$(ffprobe -i aom${cpu}_${i}.mkv 2>&1 | grep bitrate | rev | cut -d' ' -f2 | rev)" \
+            "\$(jq '.["VMAF score"]' ${i}_${j}.json)" \
+            "\$(jq '.["PSNR score"]' ${i}_${j}.json)" \
+            "\$(jq '.["SSIM score"]' ${i}_${j}.json)" \
+            "\$(jq '.["MS-SSIM score"]' ${i}_${j}.json)" | \
+            tee -a "features${1}data.txt"; \
         echo
-done
-done
+EOF
+        j=$((j + 1))
+    done
+done | parallel -u -j 4
